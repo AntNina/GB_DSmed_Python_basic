d2ccf987bbe1f923bac6ab2ec3a95d30f04e9862
Nina Antanishyna <jockobooboo@gmail.com>
2020-12-21 20:31:53 +0300

    Homework_5

---
+++ 5_task_2.py
@@ -0,0 +1,18 @@
+file_2 = open("text_2.txt", "r")
+line = 0
+for i in file_2:
+    line += 1
+
+    flag = 0
+    word = 0
+    for j in i:
+        if j != ' ' and flag == 0:
+            word += 1
+            flag = 1
+        elif j == ' ':
+            flag = 0
+
+    print(i, len(i), 'симв.', word, 'сл.')
+
+print(line, 'стр.')
+file_2.close()
